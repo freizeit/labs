cae1e7f0894512720704db3b18afeef6f558c9d7
Muharem Hrnjadovic <os@lbox.cc>
2013-10-22 15:56:47 +0200

    almost works

--- elixir/crawler/lib/crawler.ex
+++ elixir/crawler/lib/crawler.ex
@@ -14,7 +14,8 @@ defmodule Crawler do
     # IO.inspect args
     {_, uri} = List.keyfind(opts, :uri, 0)
     Tracker.init()
-    spawn(Crawler, :process_uris, [uri, self])
+    crawler_pid = spawn(Crawler, :process_uris, [uri, self])
+    crawl_uri(uri, self, crawler_pid) 
     process_results()
   end
 
@@ -22,8 +23,13 @@ defmodule Crawler do
   @doc """
     `:true` if `uri` belongs to `top_level_uri`, `:false` otherwise.
   """
-  def external_link?(top_level_uri, uri) do
-    String.contains?(uri, top_level_uri)
+  def internal_link?(top_level_uri, uri) do
+    String.contains?(uri, top_level_uri) or String.starts_with?(uri, "/")
+  end
+
+
+  defp is_html?(headers) do
+    String.contains?(ListDict.get(headers, :"Content-Type"), "text/html")
   end
 
 
@@ -32,21 +38,27 @@ defmodule Crawler do
     to the `uris` pid.
   """
   def crawl_uri(uri, results, uris) do
+    IO.puts(">> #{uri}")
     if not Tracker.crawled?(uri) do
       Tracker.register(uri)
+      IO.puts(">>> #{uri}")
       case fetch(uri) do
-        { :ok, body } ->
-          {result, uri_list} = process_page(body)
-          results <- { :ok, result }
-          uris <- uri_list
-        { :error, _ } -> { :error, uri }
+        { :ok, body, headers } ->
+          if is_html?(headers) do
+            {result, uri_list} = process_page(uri, body)
+            results <- { :ok, result }
+            uris <- uri_list
+          end
+        { :error, _, _ } -> { :error, uri }
       end
     end
   end
 
 
-  def process_page(_body) do
-    { "", [] }
+  def process_page(uri, body) do
+    links = Regex.scan(%r/<a.*href[^=]*=[^"]*"([^"]+)"/gm, body)
+    links = Enum.map(links, fn [_, uri] -> uri end)
+    { "#{uri} :: #{Enum.count(links)}", links }
   end
 
 
@@ -55,11 +67,11 @@ defmodule Crawler do
   """
   def fetch(uri) do
     case HTTPotion.get(uri, @user_agent) do
-      Response[body: body, status_code: status, headers: _headers ]
+      Response[body: body, status_code: status, headers: headers ]
       when status in 200..299 ->
-        { :ok, body }
-      Response[body: body, status_code: _status, headers: _headers ] ->
-        { :error, body }
+        { :ok, body,  headers }
+      Response[body: body, status_code: _status, headers: headers ] ->
+        { :error, body, headers }
     end
   end
 
@@ -80,10 +92,18 @@ defmodule Crawler do
   """
   def process_uris(top_level_uri, results_pid) do
     receive do
-      uri ->
-        if (external_link?(uri, top_level_uri)) do
-          spawn(Crawler, :crawl_uri, [uri, results_pid, self])
-        end
+      uris ->
+        Enum.map(uris, fn uri ->
+          IO.puts("> #{uri}")
+          if (internal_link?(uri, top_level_uri)) do
+            if String.starts_with?(uri, "/") do
+              spawn(
+                Crawler, :crawl_uri, [top_level_uri <> uri, results_pid, self])
+            else
+              spawn(Crawler, :crawl_uri, [uri, results_pid, self])
+            end
+          end
+        end)
     end
   end
 end
